2e11142e2fdc360bcca77822e5dee261e63cf54d
victorHL99 <victor.lessa@estudante.ufla.br>
2022-03-21 16:22:43 -0300

    O contador está contando, porém não está certo ainda

--- src/components/pag2/SegundaPagina.js
+++ src/components/pag2/SegundaPagina.js
@@ -2,15 +2,19 @@
 import Deck from "./Deck"
 import LogoMenor from "./LogoMenor"
 import decks from "../ComponentesGlobais"
+import React from "react"
 
 
 export default function SegundaPagina(){
+    const [contagem,setContagem]=React.useState(0)
     return (
         <main>
             <LogoMenor/>
-            <Deck/>
+            <div onClick={()=>setContagem(contagem+1)} >
+            <Deck />
+            </div>
             <footer>
-                <p>/{decks.length} CONCLUÍDOS</p>
+                <p>{contagem}/{decks.length} CONCLUÍDOS</p>
             </footer>
             
         </main>
